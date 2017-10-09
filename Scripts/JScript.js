d2ad5e70ac05e5a8c921227c45705ec59465c219
aletkolt@gmail.com <aletkolt@gmail.com>
2017-10-09 19:48:48 +0100

    Changes have been made to the files, and all the pages will be ran on the homepage with little animation. JS has been fully implemented.

---
+++ Scripts/JScript.js
@@ -0,0 +1,57 @@
+var servicesButton = $("#servicesButton");
+var contactButton = $("#contactButton");
+var indexDiv = $("#index-div");
+var contactDiv = $("#contact-div");
+var servicesDiv = $("#services-div");
+var logoAnchor = $("#logoLink");
+
+$(document).ready(function(){
+    servicesButton.click(function(){
+        showDiv("services-div");
+    });
+
+    contactButton.click(function(){
+        showDiv("contact-div");
+    });
+
+    logoAnchor.click(function(){
+        showDiv("index-div");
+    });
+});
+
+function showDiv(divisionToOpen){
+    console.log("event triggered");
+    if(divisionToOpen === "index-div"){
+        closeDiv(contactDiv);
+        closeDiv(servicesDiv);
+        openDiv(indexDiv);
+        
+    }
+    else if(divisionToOpen === "contact-div"){
+        closeDiv(indexDiv);
+        closeDiv(servicesDiv);
+        openDiv(contactDiv);
+        
+    }
+    else if(divisionToOpen === "services-div"){
+        closeDiv(contactDiv);
+        closeDiv(indexDiv);
+        openDiv(servicesDiv);
+        
+    }
+}
+
+function openDiv(division){
+    division.css("display","block");
+}
+
+function closeDiv(division){
+    division.animate({
+           opacity: 0.5,
+           height: "toggle",
+           display: "none"
+        },
+        1000, function(){
+        division.css("opacity", "1");
+    });
+}
